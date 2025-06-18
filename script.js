c4cf168106c570937d5006c35591b644e501147e
Faizal Hassan <hassanfaizal047@gmail.com>
2025-06-18 22:32:26 +0500

    Made Some Changes

--- script.js
+++
@@ -1,6 +0,0 @@
-const addTrip = document.querySelector('.addBtn')
-const addModal = document.querySelector('.addTripModal');
-
-addTrip.addEventListener('click', () => {
-    addModal.classList.toggle('show')
-})
