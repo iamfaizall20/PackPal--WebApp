91c723f8929c4cdae05e7a434eff11621ef3c98a
Faizal Hassan <hassanfaizal047@gmail.com>
2025-06-19 12:39:31 +0500

    ----

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const addTripBtn = document.querySelector('.addBtn');
 const emptySlate = document.querySelector('.emptySlate');
 const addTripPane = document.querySelector('.addTripPane');
+const viewTripsPane = document.querySelector('.viewBtn');
+const backButton = document.querySelector('.backBtn');
+const tripsPane = document.querySelector('.tripsPane');
 
 addTripBtn.addEventListener('click', () => {
     emptySlate.classList.add('hide');
+    tripsPane.classList.remove('show');
     addTripPane.classList.add('show');
-})
+});
+
+backButton.addEventListener('click', () => {
+    addTripPane.classList.remove('show');
+    tripsPane.classList.add('show');
+});
+
+viewTripsPane.addEventListener('click', () => {
+    emptySlate.classList.add('hide');
+    addTripPane.classList.remove('show');
+    tripsPane.classList.add('show');
+});
