664486b2df1f32c61dd7dd27497e451ada5f8c52
Faizal Hassan <hassanfaizal047@gmail.com>
2025-06-19 13:10:45 +0500

    Completed Add Trip Functionality

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const addTripPane = document.querySelector('.addTripPane');
 const viewTripsPane = document.querySelector('.viewBtn');
 const backButton = document.querySelector('.backBtn');
 const tripsPane = document.querySelector('.tripsPane');
+const addTodoButton = document.querySelector('.addTodo');
+const clearTodoButton = document.querySelector('.clearTodo');
 
 addTripBtn.addEventListener('click', () => {
     emptySlate.classList.add('hide');
@@ -21,3 +23,26 @@ viewTripsPane.addEventListener('click', () => {
     addTripPane.classList.remove('show');
     tripsPane.classList.add('show');
 });
+
+addTodoButton.addEventListener('click', () => {
+    const tripTodo = document.getElementById('tripTodo');
+    const taskList = document.querySelector('.tasks');
+
+    if (tripTodo.value === "") {
+        alert("fill fields")
+        return;
+    }
+
+    const Todo = document.createElement('li')
+    Todo.className = 'task-text';
+    Todo.textContent = tripTodo.value;
+
+    taskList.prepend(Todo);
+    tripTodo.value = "";
+})
+clearTodoButton.addEventListener('click', () => {
+    const taskList = document.querySelector('.tasks');
+
+    taskList.innerHTML = ''
+
+})
