684c82ea65469a87ad25a759d666e78c69adbbc5
Faizal Hassan <hassanfaizal047@gmail.com>
2025-06-20 17:03:57 +0500

    ---

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ const backButton = document.querySelector('.backBtn');
 const tripsPane = document.querySelector('.tripsPane');
 const addTodoButton = document.querySelector('.addTodo');
 const clearTodoButton = document.querySelector('.clearTodo');
+const tripData = [];
 
+window.addEventListener('load', () => {
+    if (tripData.length == 0) {
+        emptySlate.classList.remove('hide')
+    }
+})
+// Functions For Tabs 
 addTripBtn.addEventListener('click', () => {
+    addTripPane.classList.add('show');
     emptySlate.classList.add('hide');
     tripsPane.classList.remove('show');
-    addTripPane.classList.add('show');
 });
 
 backButton.addEventListener('click', () => {
@@ -24,9 +31,11 @@ viewTripsPane.addEventListener('click', () => {
     tripsPane.classList.add('show');
 });
 
+// Add Trip Button => Adds Data is Card,Array and TasksList
 addTodoButton.addEventListener('click', () => {
-    const tripTodo = document.getElementById('tripTodo');
-    const taskList = document.querySelector('.tasks');
+    const tripTitle = document.getElementById('tripTitle'); //Trip Title Field
+    const tripTodo = document.getElementById('tripTodo'); //Trip Tasks Field
+    const taskList = document.querySelector('.tasks'); //Checklist
 
     if (tripTodo.value === "") {
         alert("fill fields")
@@ -39,6 +48,21 @@ addTodoButton.addEventListener('click', () => {
 
     taskList.prepend(Todo);
     tripTodo.value = "";
+
+    // Store data in Array as Objects
+    let existingTrip = tripData.find(function (trip) {
+        return trip.Title === tripTitle.value;
+    })
+
+    if (existingTrip) {
+        existingTrip.checkLists.push(Todo.textContent)
+    } else {
+        const newTrip = {
+            Title: tripTitle.value,
+            checkLists: [Todo.textContent]
+        }
+        tripData.push(newTrip);
+    }
 })
 clearTodoButton.addEventListener('click', () => {
     const taskList = document.querySelector('.tasks');
@@ -46,3 +70,4 @@ clearTodoButton.addEventListener('click', () => {
     taskList.innerHTML = ''
 
 })
+
